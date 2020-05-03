8d1cacceca2544430483a97716a71320a4d2a14f
dmitry <yashulyak@gmail.com>
2020-05-03 11:59:05 +0300

    Add test for conflicting proposals

--- consensus/manager_test.go
+++ consensus/manager_test.go
@@ -1,6 +1,7 @@
 package consensus_test
 
 import (
+	"bytes"
 	"context"
 	"testing"
 	"time"
@@ -9,12 +10,24 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func consistent(t *testing.T, values <-chan []*types.LearnedValue, expected *types.Value, total int) {
-	n := 0
-	after := time.After(1 * time.Second)
+// consistent tests that all values are equal to the same value from the slice with expected values.
+func consistent(t *testing.T, values <-chan []*types.LearnedValue, expected []*types.Value, total int) {
+
+	var (
+		expectedID []byte
+		n          = 0
+		after      = time.After(1 * time.Second)
+	)
 	for learned := range values {
 		require.Len(t, learned, 1)
-		require.Equal(t, expected.Id, learned[0].Value.Id)
+		if expectedID == nil {
+			for _, val := range expected {
+				if bytes.Compare(val.Id, learned[0].Value.Id) == 0 {
+					expectedID = val.Id
+				}
+			}
+		}
+		require.Equal(t, expectedID, learned[0].Value.Id)
 		select {
 		case <-after:
 			require.FailNow(t, "not enough learned values")
@@ -39,9 +52,30 @@ func TestManagerNoConflictsProgress(t *testing.T) {
 
 	proposed := &types.Value{Id: []byte("first")}
 	require.NoError(t, cluster.Propose(ctx, proposed))
-	consistent(t, values, proposed, 4)
+	consistent(t, values, []*types.Value{proposed}, 4)
 
 	proposed = &types.Value{Id: []byte("second")}
 	require.NoError(t, cluster.Propose(ctx, proposed))
-	consistent(t, values, proposed, 4)
+	consistent(t, values, []*types.Value{proposed}, 4)
+}
+
+func TestManagerConflictingProgress(t *testing.T) {
+	cluster := NewCluster(4, 10*time.Millisecond, 40)
+	cluster.Start()
+	defer cluster.Stop()
+
+	values := make(chan []*types.LearnedValue, 4)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	cluster.Subscribe(ctx, values)
+
+	first := &types.Value{Id: []byte("first")}
+	second := &types.Value{Id: []byte("second")}
+	require.NoError(t, cluster.Manager(1).Propose(ctx, first))
+	require.NoError(t, cluster.Manager(2).Propose(ctx, first))
+	require.NoError(t, cluster.Manager(3).Propose(ctx, second))
+	require.NoError(t, cluster.Manager(4).Propose(ctx, second))
+
+	expected := []*types.Value{first, second}
+	consistent(t, values, expected, 4)
 }
